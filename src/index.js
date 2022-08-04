b64d8ea290e25999f9c4140a9ba3d209f9d3e4b5
Your Name <you@example.com>
2022-08-04 22:55:16 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split("");
+
+    if (str.length % 2 === 1) 
+    
+      return false;
+
+    var count = [];
+
+    for (var i = 0; i < str.length; i++) {
+      for (var y = 0; y < bracketsConfig.length; y++) { 
+        if (str[i] === bracketsConfig[y][1] && count[count.length - 1] === bracketsConfig[y][0]) {
+          count.pop(); 
+        } else if (str[i] === bracketsConfig[y][0]) {
+          count.push(str[i]); 
+        }
+      }
+    }
+
+    return count == 0;
 }
